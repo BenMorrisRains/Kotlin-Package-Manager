3b697f739c7d19e25fe4d9319d3d1aede1ce6e0b
BenMorrisRains <benmorrisrains@gmail.com>
2026-02-15 21:32:58 -0700

    Bump version to 1.0.9

--- src/main/kotlin/com/kpm/cli/Version.kt
+++ src/main/kotlin/com/kpm/cli/Version.kt
@@ -1,8 +1,8 @@
 package com.kpm.cli
 
 object Version {
-    const val VERSION = "1.0.8"
-    const val BUILD_DATE = "2024-12-06"
+    const val VERSION = "1.0.9"
+    const val BUILD_DATE = "2026-02-15"
     
     fun getVersionString(): String {
         return "KPM (Kotlin Package Manager) v$VERSION"
