dadbb973a08e4e2bdfc9b784264b6797e3f4fee0
BenMorrisRains <benmorrisrains@gmail.com>
2026-02-15 22:55:25 -0700

    Make version dynamic - auto-generate from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.time.LocalDate
+
 plugins {
     kotlin("jvm") version "2.0.0"
     application
@@ -27,5 +29,43 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+// Generate Version.kt with dynamic version from build.gradle.kts
+tasks.register("generateVersion") {
+    val versionFile = file("src/main/kotlin/com/kpm/cli/Version.kt")
+    val projectVersion = version.toString()
+    val buildDate = LocalDate.now().toString()
+    
+    doLast {
+        versionFile.writeText("""
+            package com.kpm.cli
+            
+            object Version {
+                const val VERSION = "$projectVersion"
+                const val BUILD_DATE = "$buildDate"
+                
+                fun getVersionString(): String {
+                    return "KPM (Kotlin Package Manager) v${'$'}VERSION"
+                }
+                
+                fun getFullVersionInfo(): String {
+                    return ""${'"'}
+                        KPM (Kotlin Package Manager) v${'$'}VERSION
+                        Build Date: ${'$'}BUILD_DATE
+                        
+                        Repository: https://github.com/BenMorrisRains/Kotlin-Package-Manager
+                        Documentation: https://github.com/BenMorrisRains/Kotlin-Package-Manager#readme
+                    ""${'"'}.trimIndent()
+                }
+            }
+            
+        """.trimIndent())
+    }
+}
+
+// Run generateVersion before compiling
+tasks.named("compileKotlin") {
+    dependsOn("generateVersion")
 }
 
